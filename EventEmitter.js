b82c72a603ed6abd1eee38af947cd3ff40aa0a5e
Verodant <verodant@gmail.com>
2019-09-22 21:58:45 +0200

    estrategias

--- EventEmitter.js
+++ EventEmitter.js
@@ -2,79 +2,99 @@ import Subject from './Subject.js';
 
 let INSTANCE = null;
 
+
+
 export default class EventEmitter {
-    constructor() {
-        if (INSTANCE) return INSTANCE = this;;
+
+    static get DEFAULT_VALUES() {
+        return {
+            ESTRATEGY: "LAST"
+        }
     }
 
-    on(eventName, callback) {
-        console.group(eventName);
-        const eventNameArray = eventName.split(' ');
-        console.log(eventNameArray);
-        
-        /* Si se cumple uno de los eventos */
-        eventNameArray.forEach(eventName => {
-            new Subject(eventName).addObserver(callback);  
-        });          
+    get ESTRATEGIES() {
+        return {
+            HISTORIC_ALL: (eventName, callback) => {
+                console.groupCollapsed('estrategia: HISTORIC_ALL =-> ', eventName)
+                const values = this._eventEmittedValues__Stack.get(eventName) || [];
+                console.log('valores a comunicar -> ', values);
+                values.forEach(callback);
+                console.groupEnd()
+                return callback;
+            },
+            LAST: (eventName, callback) => {
+                console.groupCollapsed('estrategia::LAST =-> ', eventName);
+                console.groupEnd();
+                return callback;
+            },
+            FIRST: (eventName, callback) => {
+                console.groupCollapsed('estrategia::FIRST =-> ', eventName)
+                const values = this._eventEmittedValues__Stack.get(eventName) || [];
+                const value = values.find(x => x !== undefined);
+                if (value) callback(value);
+                console.groupEnd();
+                return () => { };
 
-        /* Si se cumplen todos los eventos */
-        /*eventNameArray.forEach(eventName => {
+            }
+        }
+    }
 
-            //new Subject(eventName).addObserver(callback);  
-        });*/        
+    constructor() {
+        if (INSTANCE) return INSTANCE = this;
+        this._eventEmittedValues__Stack = new Map;
+    }
 
-        /* legacy */
-        //new Subject(eventName).addObserver(callback);
-        console.groupEnd();
+    on(eventName, strategy, callback = strategy) {
+        console.groupCollapsed('ON :: ', eventName);
+        console.count(`Event counter -> ${eventName}`);
+        if (typeof strategy != "string") strategy = EventEmitter.DEFAULT_VALUES.ESTRATEGY;
+        new Subject(eventName).addObserver(this.ESTRATEGIES[strategy].call(this, eventName, callback))
+        console.groupEnd()
     }
 
     emit(eventName, context = true) {
-        new Subject(eventName).status = context;
+        console.groupCollapsed('EMIT :: ', eventName);
+        console.count(`Emit ounter -> ${eventName}`);
+        if (!this._eventEmittedValues__Stack.has(eventName)) this._eventEmittedValues__Stack.set(eventName, []);
+        const values = this._eventEmittedValues__Stack.get(eventName);
+        values.push(context);
+        console.log('values -> ', values);
+        new Subject(eventName, context).notify();
+        console.groupEnd()
     }
 }
 
 const emitter = new EventEmitter;
 
-emitter.on('login cookie', (status) => {
-    console.log('los dos eventos', status)
-});
-
-emitter.on('login cookie', (status) => {
-    console.log('los dos eventos segunda vez', status)
-});
-
-emitter.on('login cookie ogt', (status) => {
-    console.log('tres eventos con ogt', status)
-});
-
-emitter.on('ogt', (status) => {
-    console.log('solo ogt', status)
-});
-
-emitter.on('login', (status) => {
-    console.log('solo login', status)
-});
-
-emitter.on('cookie', (status) => {
-    console.log('solo cookie', status)
-});
-
-
-/* Pruebas */
 emitter.emit('login');
-//emitter.emit('cookie');
-//emitter.emit('ogt');
+emitter.emit('login');
+emitter.on('login', 'FIRST', (data) => { console.log('FIRST', data) })
+emitter.on('login', (data) => { console.log('<Primer login> ', data) });
+
+emitter.emit('login');
+
+/* emitter.on('login', (data) => { console.log('<Segundo login> ', data) });
+
+emitter.emit('login');
+
+// Pruebas
+emitter.emit('login');
 
 
+*/emitter.on('login', (data) => { console.count('<Ultimo login, tiene q ser n veces> ', data) });
+
+emitter.emit('login');
+
+emitter.on('login', _ => { console.log('ultimo') && console.count('ultimo') })
+emitter.emit('login');
 
 
-/* emitter de control para saber que no lo hace siempre*/
-emitter.on('pp', (status) => {
+emitter.on('login', 'HISTORIC_ALL', _ => { console.log('historic all') && console.count('ultimo') })
+emitter.on('login', 'HISTORIC_ALL', _ => { console.log('historic all second') && console.count('ultimo') })
+
+// emitter de control para saber que no lo hace siempre
+/* emitter.on('pp', (status) => {
     console.log('soy pepe', status)
-});
-
-
-
-
-
+}); */
+emitter.emit('login');
 
---
+++ stack.js
@@ -0,0 +1,13 @@
+export class stack {
+    constructor(){
+
+    }
+
+    push(){
+        
+    }
+
+    *_stackGen(){
+
+    }
+}
